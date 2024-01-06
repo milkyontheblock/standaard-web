1478386c6166e11139c84780a261f4aeefc1c4c8
milkyontheblock <jordanvanwijnen0709@gmail.com>
2024-01-06 19:45:02 +0100

    Added temporary redirect from index to tickets page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/editions/the-after-hours-with-clubkade',
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
